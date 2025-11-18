e56f5b2c7665a6548cea3f09342b7ae9d5b74ee8
DavigadelhaDev <davigadelhadev@gmail.com>
2025-11-18 19:36:57 -0300

    Manipulating DOM with Cypress

---
+++ cypress/integration/checkBoxPractive.spec.js
@@ -0,0 +1,17 @@
+describe('Check Box Practice', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    })
+
+    it('Select the checkbox', () => {
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1');
+    });
+
+    it('Unselect the checkbox', () => {
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked');
+    });
+
+    it('Select multiples checkbox', () => {
+        cy.get('input[type="checkbox"]').check(['option2', 'option3']);
+    });
+});
